631c7e7be12166a15cbd5caf068be2a17538c732
cgrisales04 <cristiangrisales04@gmail.com>
2023-06-22 23:17:29 -0500

    Modificacion de Login - Autenticacion con Google y/o servicio REST

--- models/server.js
+++ models/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
+const { createServer } = require("http");
 
 const { dbConnection } = require("../database/config");
+const { socketController } = require("../sockets/socket.controller");
 
 class Server {
   constructor() {
     this.app = express();
     this.port = process.env.PORT;
+    this.server = createServer(this.app);
+    this.io = require("socket.io")(this.server);
 
     this.paths = {
       auth: "/api/auth",
@@ -26,6 +30,9 @@ class Server {
 
     // Rutas de mi aplicación
     this.routes();
+
+    //Sockets
+    this.sockets();
   }
 
   async conectarDB() {
@@ -60,9 +67,12 @@ class Server {
     this.app.use(this.paths.usuarios, require("../routes/usuarios"));
     this.app.use(this.paths.uploads, require("../routes/uploads"));
   }
+  sockets() {
+    this.io.on("connection", socketController);
+  }
 
   listen() {
-    this.app.listen(this.port, () => {
+    this.server.listen(this.port, () => {
       console.log("Servidor corriendo en puerto", this.port);
     });
   }
